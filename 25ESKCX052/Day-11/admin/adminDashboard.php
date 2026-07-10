d7ccae94005cdbab3e4fb78b60311fb97c192e97
kritika <ksaini0410@gmail.com>
2026-07-10 20:22:12 +0530

    Day11 assignments

---
+++ 25ESKCX052/Day-11/admin/adminDashboard.php
@@ -0,0 +1,19 @@
+<?php
+include ('dashboardHead.php');
+include ('dashboardVertical.php');
+?>
+
+<div class ="container-fluid">
+
+<div class = "row">
+
+ <div class = "col-md-9">
+
+ <h2> Admin Dashboard <br><?php echo "Welcome, " . $_SESSION['user_name'] . "!"; ?></h2>
+
+ </div>
+
+ </div>
+
+ </div>
+
---
+++ 25ESKCX052/Day-7/test.php
@@ -0,0 +1,22 @@
+<?php
+echo "Hi", "World", "I", "Am <br>";
+print "Hi"."World <br>";
+print("Hi");
+$age = 18;
+if($age>=18){
+    print("Adult");
+}else{
+    print("Teenager");
+}
+
+echo "<br>";
+
+
+($age>=18)? print("Adult") : print("Teenager");
+
+
+if(print("true <br>")){
+    print("false <br>");
+}
+
+?>
